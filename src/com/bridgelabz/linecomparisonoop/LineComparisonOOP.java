c55dbb847826a24cd4f427e25c8a9dacab327346
erakshaykumar <akaakshay304@gmail.com>
2022-02-12 22:56:48 +0530

    Added: Welcome To Line Comparison Problems

---
+++ src/com/bridgelabz/linecomparisonoop/LineComparisonOOP.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linecomparisonoop;
+
+public class LineComparisonOOP {
+
+	public static void main(String[] args) {
+		// print Welcome Message
+		System.out.println("Welcome To Line Comparison Using OOP's Concept");
+	}
+
+}
