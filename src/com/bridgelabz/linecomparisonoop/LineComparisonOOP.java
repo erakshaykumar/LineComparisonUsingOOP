c2e89d9a37083e3d805892cfa21e2b82c72ef492
erakshaykumar <akaakshay304@gmail.com>
2022-02-22 17:32:06 +0530

    Added: Compare Two Line Lengths Using EqualTo Method

--- src/com/bridgelabz/linecomparisonoop/LineComparisonOOP.java
+++ src/com/bridgelabz/linecomparisonoop/LineComparisonOOP.java
@@ -5,51 +5,54 @@ import java.util.Scanner;
 class Coordinates {
 	Scanner sc = new Scanner(System.in);
 	// instance variables
-	int x1, x2, x3, x4, y1, y2, y3, y4;
-	Double length1, length2;
+	static double x1, x2, y1, y2, x3, x4, y3, y4;
+	static int distance1, distance2;
 
-	/*
-	 * taking inputs for first line
-	 */
-	public void inputLine1() {
-		System.out.println("Enter the Co-ordinates of x1 and y1 : ");
-		x1 = sc.nextInt();
-		y1 = sc.nextInt();
-
-		System.out.println("Enter the Co-ordinates of x2 and y2 : ");
-		x2 = sc.nextInt();
-		y2 = sc.nextInt();
-
-		length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-		System.out.println("Length of the given Line1 is : " + length1 + " units");
+	// Computation for point1 & point2
+	public void point1() {
+		System.out.println("enter x1 & y1 values of first point");
+		x1 = sc.nextDouble();
+		y1 = sc.nextDouble();
 	}
 
-	/*
-	 * taking inputs for second line
-	 */
-	public void inputLine2() {
-		System.out.println("Enter the Co-ordinates of x3 and y3 : ");
-		x3 = sc.nextInt();
-		y3 = sc.nextInt();
-
-		System.out.println("Enter the Co-ordinates of x4 and y4 : ");
-		x4 = sc.nextInt();
-		y4 = sc.nextInt();
-
-		length2 = Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
-		System.out.println("Length of the given Line2 is : " + length2 + " units");
+	public void point2() {
+		System.out.println("enter x2 & y2 values of second point");
+		x2 = sc.nextDouble();
+		y2 = sc.nextDouble();
 	}
 
-	/*
-	 * Comparing both lines using compareTo method
-	 */
-	public void compare() {
-		if (length1.compareTo(length2) == 1)
-			System.out.println("Length of Line 1 is greater than Line 2 ");
-		else if (length1.compareTo(length2) == -1)
-			System.out.println("Length of Line 1 is less than Line 2 ");
+	public void distanceBetweenPoint1Point2() {
+		distance1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("Distance between two points = " + distance1);
+	}
+
+	// Computation for point3 & point4
+	public void point3() {
+		System.out.println("enter x3 & y3 values of third point");
+		x3 = sc.nextDouble();
+		y3 = sc.nextDouble();
+	}
+
+	public void point4() {
+		System.out.println("enter x2 & y2 values of second point");
+		x4 = sc.nextDouble();
+		y4 = sc.nextDouble();
+	}
+
+	public void distanceBetweenPoint3Point4() {
+		distance2 = (int) Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+		System.out.println("Distance between two points = " + distance2);
+	}
+
+	// checking for line equality
+	public void CompareLinesUsingEqualToMethod() {
+		String str1 = Double.toString(distance1);
+		String str2 = Double.toString(distance2);
+
+		if (str1.equals(str2) == true)
+			System.out.println("Both lines are equal");
 		else
-			System.out.println("Both lines are equal in length");
+			System.out.println("Not both lines are equal");
 	}
 }
 
@@ -57,9 +60,13 @@ public class LineComparisonOOP {
 	public static void main(String[] args) {
 		Coordinates coordinate = new Coordinates();
 		// Base class method calling
-		coordinate.inputLine1();
-		coordinate.inputLine2();
-		coordinate.compare();
+		coordinate.point1();
+		coordinate.point2();
+		coordinate.distanceBetweenPoint1Point2();
+		coordinate.point3();
+		coordinate.point4();
+		coordinate.distanceBetweenPoint3Point4();
+		coordinate.CompareLinesUsingEqualToMethod();
 	}
 
 }
